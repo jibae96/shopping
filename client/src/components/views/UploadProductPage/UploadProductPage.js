d44213db7824d39069546960df20c331b6cfae8f
jungin <jibae96@naver.com>
2020-11-04 14:24:08 +0900

    add title, description, price, category part

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Typography, Button, Form, Input } from 'antd';
+
+const { TextArea } = Input;
+
+const CategoryName = [
+    { key:1, value:"Tables" },
+    { key:2, value:"Seats" },
+    { key:3, value:"Shelves" },
+    { key:4, value:"Lighting" },
+    { key:5, value:"Accessories" }
+]
 
 function UploadProductPage(){
+
+    const [Title, setTitle] = useState("")
+    const [Description, setDescription] = useState("")
+    const [Price, setPrice] = useState(0)
+    const [Category, setCategory] = useState(1)
+    const [Images, setImages] = useState([])
+
+    const titleChangeHandler = (event) => {
+        setTitle(event.currentTarget.value)
+    }
+
+    const descriptionChangeHandler = (event) => {
+        setDescription(event.currentTarget.value)
+    }
+
+    const priceChangeHandler = (event) => {
+        setPrice(event.currentTarget.value)
+    }
+
+    const categoryChangeHandler = (event) => {
+        setCategory(event.currentTarget.value)
+    }
+
     return (
-        <div>
-            UploadProductPage
+        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+            <div style={{textAlign: 'center', marginBottom: '2rem'}}>
+                <h2>가구 상품 업로드</h2>
+            </div>
+            <Form>
+                {/* DropZone */}
+                <br/>
+                <br/>
+                <label>이름</label>
+                <Input onChange={titleChangeHandler} value={Title}/>
+                <br/>
+                <br/>
+                <label>설명</label>
+                <TextArea onChange={descriptionChangeHandler} value={Description}/>
+                <br/>
+                <br/>
+                <label>가격($)</label>
+                <Input type="number" onChange={priceChangeHandler} value={Price}/>
+                <br/>
+                <br/>
+                <select onChange={categoryChangeHandler}>
+                    {CategoryName.map(item => (
+                        <option key={item.key} value={Category}> {item.value} </option>
+                    ))}
+                </select>
+                <br/>
+                <br/>
+                <Button>
+                    확인   
+                </Button>
+            </Form>
         </div>
     )
 }
